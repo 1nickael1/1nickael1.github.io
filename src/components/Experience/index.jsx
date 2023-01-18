92e6865cf704c3e47f89e3f1b32d0897f562c23b
Nickael <nbruzzi@sonco.com>
2023-01-18 19:43:49 -0300

    fix portfolio

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -28,40 +28,29 @@ export const Experience = forwardRef(function Experience(props, ref) {
                 <h1>experiência</h1>
                 <div className="options">
                     <div className={actualBanner == 0 ? 'option active' : 'option'} onClick={() => setNewBanner(0)}>
-                        Estágio
+                        Pleno EUA
                     </div>
                     <div className={actualBanner == 1 ? 'option active' : 'option'} onClick={() => setNewBanner(1)}>
-                        Júnior
-                    </div>
-                    <div className={actualBanner == 2 ? 'option active' : 'option'} onClick={() => setNewBanner(2)}>
                         Pleno BR
                     </div>
+                    <div className={actualBanner == 2 ? 'option active' : 'option'} onClick={() => setNewBanner(2)}>
+                        Júnior
+                    </div>
                     <div className={actualBanner == 3 ? 'option active' : 'option'} onClick={() => setNewBanner(3)}>
-                        Pleno EUA
+                        Estágio
                     </div>
                 </div>
             </div>
             <div className="container-scroll" ref={bannerRef}>
                 <div className="details">
                     <div className="title">
-                        Front-end Mobile <span>Ago 2019 - Jan 2020</span>
+                        Front-end <span>Set 2021 - Atual</span>
                     </div>
                     <p className="company">
-                        Marcas na Web
+                        Sonco Worldwide
                     </p>
                     <p className="resume">
-                        Aplicativos em React Native
-                    </p>
-                </div>
-                <div className="details">
-                    <div className="title">
-                        Full-Stack <span>Jan 2020 - Abr 2020</span>
-                    </div>
-                    <p className="company">
-                        Tech.In
-                    </p>
-                    <p className="resume">
-                        Front-end com React, Back-end com Node e PostgreSQL
+                        Front-end com Vue, React e React Native
                     </p>
                 </div>
                 <div className="details">
@@ -77,13 +66,24 @@ export const Experience = forwardRef(function Experience(props, ref) {
                 </div>
                 <div className="details">
                     <div className="title">
-                        Front-end <span>Set 2021 - Atual</span>
+                        Full-Stack <span>Jan 2020 - Abr 2020</span>
                     </div>
                     <p className="company">
-                        Sonco Worldwide
+                        Tech.In
                     </p>
                     <p className="resume">
-                        Front-end com Vue, React e React Native
+                        Front-end com React, Back-end com Node e PostgreSQL
+                    </p>
+                </div>
+                <div className="details">
+                    <div className="title">
+                        Front-end Mobile <span>Ago 2019 - Jan 2020</span>
+                    </div>
+                    <p className="company">
+                        Marcas na Web
+                    </p>
+                    <p className="resume">
+                        Aplicativos em React Native
                     </p>
                 </div>
             </div>
